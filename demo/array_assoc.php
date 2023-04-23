a7281f857371c20177d0816a5c2ae0928886ddc1
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-23 19:08:42 +0200

    Add new file

---
+++ demo/array_assoc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$number = array(10, 20, 49);
+
+echo $number[2] . "<br>";
+
+$names = array("Maria", "Josef", "Albert");
+
+$text = array("first_name" => 'Edward'); // => assigning a element
+
+?>
+
+
+</body>
+
+</html>
