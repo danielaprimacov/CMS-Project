0cbc3129ed1731564988bf6146d8083bbf183095
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-23 18:56:21 +0200

    Add new file
    Working with arrays in PHP

---
+++ demo/array.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php 
+
+$numberList1 = array(1, 2, 3, 55, '<h1> Hi </h1>'); // initializing array
+
+$numberList2 = [1, 4, 5, 55, '<h1> Hello </h1>']; // another option of initializing array
+
+// print_r($numberList2); // print an array
+
+echo $numberList1[4]; // print a specific element of an array
+
+?>
+
+
+
+
+</body>
+
+</html>
