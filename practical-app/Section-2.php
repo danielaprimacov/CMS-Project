8107ff9cde24af827b4b153b386b47f75a4e42b3
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-24 12:27:42 +0200

    Add new file

---
+++ practical-app/Section-2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+// make 2 variables and set value 10 and 20
+$number1 = 10;
+$number2 = 20;
+
+// add 2 variables and display the sum
+echo $number1 + $number2;
+
+echo "<br>";
+
+// make 2 arrays with the same values
+// first regulat
+$firstArray = array("Daniela");
+// second associative
+$secondArray = ["firstName" => 'Daniela'];
+
+// display those 2 arrays
+print_r($firstArray);
+echo "<br>";
+print_r($secondArray);
+
+?>
+
+
+</body>
+
+</html>
