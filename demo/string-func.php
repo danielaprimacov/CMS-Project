269d6f72a7a121145d123c89e7bfa227829d11f7
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-24 15:05:31 +0200

    Add new file

---
+++ demo/string-func.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$string = "Heloo there";
+
+echo strlen($string) . "<br>";
+
+echo strtoupper($string) . "<br>";
+
+echo strtolower($string) . "<br>";
+
+?>
+
+
+
+
+</body>
+
+</html>
