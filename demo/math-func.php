67bdf3db16289830dd721e330c3a14b5979fa0a2
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-24 14:56:03 +0200

    Add new file

---
+++ demo/math-func.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php 
+
+// 2 ^ (something)
+
+echo pow(2, 7) . "<br>";
+
+// random number
+echo rand(1, 100) . "<br>";
+
+// sqrt
+echo sqrt(100) . "<br>";
+
+// round up
+echo ceil(4.6) . "<br>";
+
+// round down
+echo floor(4.6) . "<br>";
+
+// round 
+echo round(4.5) . "<br>";
+
+?>
+
+
+
+
+</body>
+
+</html>
