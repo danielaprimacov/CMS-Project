ae391d74912c06df7316dd71a029da9f416c5c44
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-24 13:46:25 +0200

    Add new file

---
+++ demo/foreach-loop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$numbers = array(345, 397, 458, 787, 145, 563, 25478);
+
+foreach($numbers as $number) {
+    echo $number . " ";
+}
+
+?>
+
+
+
+
+</body>
+
+</html>
