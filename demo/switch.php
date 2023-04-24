979287d635ff150b64bda7160516ba7cc4bd0afe
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-24 12:56:02 +0200

    Add new file

---
+++ demo/switch.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$number = 14;
+
+switch($number) {
+    case 34:
+        echo "it is 34";
+        break;
+    case 37:
+        echo "it is 34";
+        break;
+    case 35:
+        echo "it is 35";
+        break;
+    case 24:
+        echo "it is 24";
+        break;
+    default:
+        echo "it is not here";
+        break;
+}
+
+?>
+
+
+
+
+</body>
+
+</html>
