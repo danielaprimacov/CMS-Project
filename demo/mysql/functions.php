7919767af12abae6c02d77fc37d63dc4dd89eac3
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-27 22:25:56 +0200

    Update functions.php

--- demo/mysql/functions.php
+++ demo/mysql/functions.php
@@ -16,21 +16,75 @@ function showData() {
   }
 }
 
+function readData() {
+  global $connection;
+  $query = "SELECT * FROM users";
+  $result = mysqli_query($connection, $query);
+
+  if(!$result) {
+    die('Query failed');
+  }
+
+}
+
 function updateTable() {
+    if (isset($_POST['submit'])) {
+      global $connection;
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $id = $_POST['id'];
+
+      $query = "UPDATE users SET ";
+      $query .= "username = '$username', ";
+      $query .= "password = '$password' ";
+      $query .= "WHERE id = $id";
+
+      $result = mysqli_query($connection, $query);
+      if(!$result) {
+        die("Query failed");
+      }
+   }
+}
+
+
+function deleteUser() {
+  if (isset($_POST['submit'])) {
     global $connection;
-    $username = $_POST['username'];
-    $password = $_POST['password'];
     $id = $_POST['id'];
 
-    $query = "UPDATE users SET ";
-    $query .= "username = '$username', ";
-    $query .= "password = '$password' ";
+    $query = "DELETE FROM users ";
     $query .= "WHERE id = $id";
 
     $result = mysqli_query($connection, $query);
-    if(!$result) {
+    if (!$result) {
       die("Query failed");
+    } else {
+      echo "User deleted!";
     }
+  }
 }
+
+
+function createUser(){
+  global $connection;
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $query = "INSERT INTO users(username, password) ";
+  $query .= "VALUES ('$username', '$password')";
+
+  $result = mysqli_query($connection, $query);
+
+  if(!$result) {
+    die('Query failed');
+  } else {
+    echo "Query created!";
+  }
+
+  if(!$username && !$password) {
+    echo "Must provide username and password!";
+  }
+}
+
 
 ?>
