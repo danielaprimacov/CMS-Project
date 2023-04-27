e53ed227304774aa8a8b9734ffcdd14695b24a5f
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-27 14:17:06 +0200

    Update login_create.php

--- demo/mysql/login_create.php
+++ demo/mysql/login_create.php
@@ -45,13 +45,14 @@ if(isset($_POST['submit'])) {
 
 <div class="container">
   <div class="col-sm-6">
+    <h1 class="text-center">Create User</h1>
     <form action="login_create.php" method="post"> 
       <div class="form-group">
-        <label for="username">Username</label>
+        <label class="form-label" for="username">Username</label>
         <input type="text" class="form-control" name="username" placeholder="Username">
       </div>
       <div class="form-group">
-        <label for="password">Password</label>
+        <label class="form-label" for="password">Password</label>
         <input type="password" class="form-control" name="password" placeholder="Password">
       </div>
       <input class="btn btn-primary mt-3" type="submit" name="submit" value="Submit"> 
