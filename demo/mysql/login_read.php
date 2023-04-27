069769f38949ef52ba4b99f1c6cc9a1c1d15e138
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-27 22:27:21 +0200

    Update login_read.php

--- demo/mysql/login_read.php
+++ demo/mysql/login_read.php
@@ -1,36 +1,9 @@
-<?php 
+<?php include "db.php";?>
+<?php include "functions.php";?>
 
-  $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
-  if($connection) {
-    echo "Connected";
-  } else {
-    die("Database connetion failed");
-  }
+<?php readData();?>
 
-  $query = "SELECT * FROM users";
-
-  $result = mysqli_query($connection, $query);
-
-  if(!$result) {
-    die('Query failed');
-  }
-
-
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Login App</title>
-</head>
-
-<body>
+<?php include "includes/header.php";?>
 
 <div class="container">
   <div class="col-sm-6">
@@ -50,9 +23,4 @@
   </div>
 </div>
 
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php include "includes/footer.php";?>
