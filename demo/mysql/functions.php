85913af13a8d1675643deff07fdd1fa5d65e16fa
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-27 13:50:32 +0200

    Update functions.php

--- demo/mysql/functions.php
+++ demo/mysql/functions.php
@@ -16,4 +16,21 @@ function showData() {
   }
 }
 
+function updateTable() {
+    global $connection;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $id = $_POST['id'];
+
+    $query = "UPDATE users SET ";
+    $query .= "username = '$username', ";
+    $query .= "password = '$password' ";
+    $query .= "WHERE id = $id";
+
+    $result = mysqli_query($connection, $query);
+    if(!$result) {
+      die("Query failed");
+    }
+}
+
 ?>
