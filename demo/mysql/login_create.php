3ccc628aac86cc6281857e47a8fa870928572f03
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-27 22:26:22 +0200

    Update login_create.php

--- demo/mysql/login_create.php
+++ demo/mysql/login_create.php
@@ -1,47 +1,13 @@
-<?php 
+<?php include "db.php"; ?>
+<?php include "functions.php"; 
 
 if(isset($_POST['submit'])) {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
-  if($connection) {
-    echo "Connected";
-  } else {
-    die("Database connetion failed");
-  }
-
-  $query = "INSERT INTO users(username, password) ";
-  $query .= "VALUES ('$username', '$password')";
-
-  $result = mysqli_query($connection, $query);
-
-  if(!$result) {
-    die('Query failed');
-  }
-
-  if($username && $password) {
-    echo "";
-  } else {
-    echo "Must provide username and password!";
-  }
+  createUser();
 }
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Login App</title>
-</head>
-
-<body>
+<?php include "includes/header.php";?>
 
 <div class="container">
   <div class="col-sm-6">
@@ -55,14 +21,10 @@ if(isset($_POST['submit'])) {
         <label class="form-label" for="password">Password</label>
         <input type="password" class="form-control" name="password" placeholder="Password">
       </div>
-      <input class="btn btn-primary mt-3" type="submit" name="submit" value="Submit"> 
+      <input class="btn btn-primary mt-3" type="submit" name="submit" value="Create"> 
     </form>
   </div>
 </div>
 
+<?php include "includes/footer.php";?>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-</body>
-
-</html>
