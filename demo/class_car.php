51e8870ca2c304fc5a7a235025d48565f5d9246f
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-29 13:47:17 +0200

    Update class_car.php

--- demo/class_car.php
+++ demo/class_car.php
@@ -2,19 +2,31 @@
 
 class Car
 {
+  // Adding proprieties
+  var $wheel = 4;
+  var $hood = 1;
+  var $engine = 1;
+  var $doors = 4;
   function moveWheels()
   {
-    echo "Wheels move!";
+    $this->wheel = 4; // this is used for refering to class inside it
+  }
+
+  function createDoors() {
+    $this->doors = 6;
   }
 }
 
-if (method_exists("Car", "moveWheels")) {
-  echo "Yes!";
-}
-
 $car = new Car(); // create an object -- instance of class Car
+$truck = new Car();
 
 $car->moveWheels();
+echo "This car has " . $car->wheel ." wheels" . "<br>";
 
+echo "Truck has " . $truck->wheel = 10 . " wheels" . "<br>";
+
+$truck->createDoors();
+
+echo "Truck has " . $truck->doors . " doors";
 
 ?>
