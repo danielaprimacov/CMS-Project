36b0932eda79bb74d664ffe8978b8355a151d563
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-04-29 13:53:16 +0200

    Add new file

---
+++ demo/class_inheritance.php
@@ -0,0 +1,31 @@
+<?php
+
+class Car
+{
+  // Adding proprieties
+  var $wheel = 4;
+  var $hood = 1;
+  var $engine = 1;
+  var $doors = 4;
+  function moveWheels()
+  {
+    $this->wheel = 4; // this is used for refering to class inside it
+  }
+
+  function createDoors() {
+    $this->doors = 6;
+  }
+}
+
+$car = new Car(); // create an object -- instance of class Car
+
+
+class Plane extends Car{
+
+}
+
+$jet = new Plane();
+
+echo $jet->wheel;
+
+?>
