e1a9c062696499d47ea12cad2f15b74aa02acc21
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-05-01 17:08:52 +0200

    Add new file

---
+++ main/includes/search-footer.php
@@ -0,0 +1,16 @@
+    <!-- Navigation -->
+    <?php include "includes/navigation.php"; ?>
+
+    <!-- Page Content -->
+    <?php include "includes/search-content.php"; ?>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    </body>
+
+    </html>
