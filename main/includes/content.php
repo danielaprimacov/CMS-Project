facb731e96f4c874a7191a67783fe0c28839b51b
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-05-01 12:13:36 +0200

    Update content.php

--- main/includes/content.php
+++ main/includes/content.php
@@ -1,9 +1,25 @@
 <div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <?php
+
+            $query = "SELECT * FROM posts";
+            $selectAllPosts = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_assoc($selectAllPosts)) {
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_img = $row['post_img'];
+                $post_content = $row['post_content'];
+            ?>
+
+
+
 
                 <h1 class="page-header">
                     Page Heading
@@ -12,39 +28,41 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2022 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/blog-sidebar.php"; ?>
+            <?php } ?>
 
         </div>
-        <!-- /.row -->
 
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2023</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/blog-sidebar.php"; ?>
 
     </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2023</p>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </footer>
+
+</div>
