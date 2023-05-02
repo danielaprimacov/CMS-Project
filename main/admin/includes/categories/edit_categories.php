44391ba9e51a8b8f0e5a3df213834c8bf1c60c2e
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-05-02 17:58:37 +0200

    Add files via upload

---
+++ main/admin/includes/categories/edit_categories.php
@@ -0,0 +1,40 @@
+<form action="" method="post">
+  <div class="form-group">
+    <label class="form-label" for="category_title">Update Category</label>
+    <?php
+
+    if (isset($_GET['edit'])) {
+      $cat_id_edit = $_GET['edit'];
+      $query = "SELECT * FROM categories WHERE cat_id = $cat_id_edit";
+      $selectCaterogiesEdit = mysqli_query($connection, $query);
+
+      while ($row = mysqli_fetch_assoc($selectCaterogiesEdit)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+    ?>
+        <input type="text" class="form-control" name="cat_title" value="
+            <?php if (isset($cat_title)) {
+              echo $cat_title;
+            } ?> ">
+      <?php } ?>
+    <?php } ?>
+
+    <?php
+
+    if (isset($_POST['edit'])) {
+      $cat_title_edit = $_GET['category_title'];
+      $query = "UPDATE categorie SET cat_title = '{$cat_title_edit}' WHERE cat_id = '{$cat_id}'";
+      $updateQuery = mysqli_query($connection, $query);
+
+      if (!$updateQuery) {
+        die("Query failed" . mysqli_error($connection));
+      }
+    }
+
+    ?>
+
+  </div>
+  <div class="form-group">
+    <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
+  </div>
+</form>
