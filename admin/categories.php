eb77e4d2eb123665719add9371872d76f132d2db
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2023-05-04 19:45:34 +0200

    Delete admin directory

--- admin/categories.php
+++
@@ -1,6 +0,0 @@
-<?php include "../includes/db.php"; ?>
-<?php include "includes/header.php"; ?>
-<?php include "functions.php"; ?>
-<?php include "includes/categories/footer-categories.php"; ?>
-
-    
--- admin/comments.php
+++
@@ -1,6 +0,0 @@
-<?php include "../includes/db.php"; ?>
-<?php include "includes/header.php"; ?>
-<?php include "functions.php"; ?>
-<?php include "includes/comments/footer-comments.php"; ?>
-
-    
