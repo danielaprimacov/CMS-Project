718f4524e0e14445d768a2798ed268f2e1a939d6
Daniela <daniela.primacov@yahoo.com>
2023-05-09 18:19:09 +0200

    add modified file

--- main/admin/includes/posts/view-all-posts.php
+++ main/admin/includes/posts/view-all-posts.php
@@ -1,42 +1,92 @@
-<?php 
-  if(isset($_GET['delete'])){
-    $post_id_delete = $_GET['delete'];
-    $query = "DELETE FROM posts WHERE post_id = {$post_id_delete}";
-    $deleteQuery = mysqli_query($connection, $query);
+<?php
+if (isset($_GET['delete'])) {
+  $post_id_delete = $_GET['delete'];
+  $query = "DELETE FROM posts WHERE post_id = {$post_id_delete}";
+  $deleteQuery = mysqli_query($connection, $query);
 
-    if(!$deleteQuery) {
-      die("Query failed!" . mysqli_error($connection));
-    } else {
-      echo "<div class='alert alert-success' role='alert'>Post Successfully Deleted!</div>";
-    }
-
+  if (!$deleteQuery) {
+    die("Query failed!" . mysqli_error($connection));
+  } else {
+    echo "<div class='alert alert-success' role='alert'>Post Successfully Deleted!</div>";
   }
+}
 ?>
 
-<table class="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Author</th>
-          <th>Title</th>
-          <th>Content</th>
-          <th>Category</th>
-          <th>Status</th>
-          <th>Image</th>
-          <th>Tags</th>
-          <th>Comments</th>
-          <th>Date</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
+<?php
+if (isset($_POST['checkBoxArray'])) {
+  foreach ($_POST['checkBoxArray'] as $checkboxElement) {
+    $bulk_options = $_POST['bulk_options'];
+    switch($bulk_options) {
+      case 'Draft':
+        $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = $checkboxElement";
+        $draftQuery = mysqli_query($connection, $query);
+        if(!$draftQuery) {
+          die("Query failed!" . mysqli_error($connection));
+        }
+        break;
+      case 'Publish':
+        $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = $checkboxElement";
+        $publishQuery = mysqli_query($connection, $query);
+        if(!$publishQuery) {
+          die("Query failed!" . mysqli_error($connection));
+        }
+        break;
+      case 'delete':
+        $query = "DELETE FROM posts WHERE post_id = $checkboxElement";
+        $deleteQuery = mysqli_query($connection, $query);
+        if(!$deleteQuery) {
+          die("Query failed!" . mysqli_error($connection));
+        }
+        break;
+      default:
+        echo '';
+        break;
+    }
+  }
+}
+?>
+
+
+<form action="" method="post">
+
+  <table class="table table-bordered table-hover">
+    <div id="bulkOptionsContainer" class="col-xs-4">
+      <select class="form-control" style="margin-bottom: 10px;" name="bulk_options">
+        <option value="Default">Choose...</option>
+        <option value="Published">Publish</option>
+        <option value="Draft">Draft</option>
+        <option value="Delete">Delete</option>
+      </select>
+    </div>
+    <div class="col-xs-4">
+      <input type="submit" name="submit" class="btn btn-success" value="Apply">
+      <a class="btn btn-primary" href="add-post.php">Add New Post</a>
+    </div>
+
+    <thead>
+      <tr>
+        <th><input class="checkBoxes" type="checkbox"></th>
+        <th>Id</th>
+        <th>Author</th>
+        <th>Title</th>
+        <th>Content</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Comments</th>
+        <th>Date</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
       <?php
 
       $query = "SELECT * FROM posts";
       $selectAllPosts = mysqli_query($connection, $query);
 
-      while($row = mysqli_fetch_assoc($selectAllPosts)) {
+      while ($row = mysqli_fetch_assoc($selectAllPosts)) {
         $post_id = $row['post_id'];
         $post_author = $row['post_author'];
         $post_content = $row['post_content'];
@@ -48,33 +98,35 @@
         $post_comments = $row['post_comments_count'];
         $post_date = $row['post_date'];
       ?>
-      <tr>
-        <td><?php echo $post_id; ?></td>
-        <td><?php echo $post_author; ?></td>
-        <td><?php echo $post_title; ?></td>
-        <td><?php echo $post_content; ?></td>
+        <tr>
+          <td><input type="checkbox" id="selectAllBoxes" name="checkBoxArray[]" value="<?php echo $post_id; ?>"></td>
+          <td><?php echo $post_id; ?></td>
+          <td><?php echo $post_author; ?></td>
+          <td><?php echo $post_title; ?></td>
+          <td><?php echo $post_content; ?></td>
 
-        <?php 
+          <?php
           $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
           $selectAllCategories = mysqli_query($connection, $query);
 
-          while($row = mysqli_fetch_assoc($selectAllCategories)) {
+          while ($row = mysqli_fetch_assoc($selectAllCategories)) {
             $cat_id = $row['cat_id'];
             $cat_title = $row['cat_title'];
 
             echo "<td>{$cat_title}</td>";
           }
-        ?>
+          ?>
 
-        <td><?php echo $post_status; ?></td>
-        <td><img class='img-responsive' src='../images/<?php echo $post_img; ?>'></td>
-        <td><?php echo $post_tags; ?></td>
-        <td><?php echo $post_comments; ?></td>
-        <td><?php echo $post_date; ?></td>
-        <td><a href='posts.php?source=edit-post&p_id=<?php echo $post_id; ?>'>Edit</a></td>
-        <td><a href='posts.php?delete=<?php echo $post_id; ?>'>Delete</a></td>
-      </tr>
-     <?php } ?>
+          <td><?php echo $post_status; ?></td>
+          <td><img class='img-responsive' src='../images/<?php echo $post_img; ?>'></td>
+          <td><?php echo $post_tags; ?></td>
+          <td><?php echo $post_comments; ?></td>
+          <td><?php echo $post_date; ?></td>
+          <td><a href='posts.php?source=edit-post&p_id=<?php echo $post_id; ?>'>Edit</a></td>
+          <td><a href='posts.php?delete=<?php echo $post_id; ?>'>Delete</a></td>
+        </tr>
+      <?php } ?>
 
-      </tbody>
-    </table>
+    </tbody>
+  </table>
+</form>
