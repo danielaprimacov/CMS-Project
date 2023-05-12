b9e83b5b27673dee8dcf7b35d36cde123542ac12
Daniela <daniela.primacov@yahoo.com>
2023-05-12 12:11:42 +0200

    Add modified file

--- main/admin/includes/main-content.php
+++ main/admin/includes/main-content.php
@@ -1,5 +1,25 @@
 <div id="wrapper">
 
+<?php
+$session = session_id();
+$time = time();
+$time_out_sec = 60;
+$time_out = $time - $time_out_sec;
+
+$query = "SELECT * FROM users_online WHERE session = '$session'";
+$querySession = mysqli_query($connection, $query);
+if(!$querySession) {
+  die("Query failed!" . mysqli_error($connection));
+}
+
+$count = mysqli_num_rows($querySession);
+if(!$count == NULL) {
+  mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+} else {
+  mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+}
+?>
+
   <!-- Navigation -->
   <?php include "navigation.php"; ?>
   <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
